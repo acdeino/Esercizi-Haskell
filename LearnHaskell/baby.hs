276180daa05895b092c6e17381f0b7186d6e8101
namlose <namlose@github.com>
2018-11-09 01:34:10 +0100

    baby.hs

--- LearnHaskell/baby.hs
+++ LearnHaskell/baby.hs
@@ -1,2 +1,8 @@
-doubleme x = x + x
-doubleus xy = x + x + y + y
+doubleMe x = x + x
+
+doubleUs x y = x*2 + y*2
+-- doubleUs x y = doubleMe x + doubleMe y would be good as well, although its presence with the same function (described differently)could be interpreted as "redundant" ->  warning: [-Woverlapping-patterns]
+
+doubleSmallNumber x = if x > 100
+  then x
+  else x*2
